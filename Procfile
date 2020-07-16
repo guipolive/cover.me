032108942835535c9a1d78e811c5b77f8943af3f
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-07-16 00:14:12 -0300

    separate frontend and backend in git submodules || remove old files

--- Procfile
+++
@@ -1,1 +0,0 @@
-backend: npm run backend
--- frontend/src/services/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'; // axios vai nos permitir conversar com o backend
-
-// const api = axios.create({baseURL: 'http://localhost:3001/api'}); // aqui fica a conexão com o backend, e deve ficar o link para ele
-const api = axios.create({baseURL: 'https://coverme-backend.herokuapp.com/api'}); // aqui fica a conexão com o backend, e deve ficar o link para ele
-
-export default api; // usarmemos 'api' sempre que formos fazer conexão com a api
